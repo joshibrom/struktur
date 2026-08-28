26d59bbbc175edb9ac60acac9a6d5f67d4c855b6
Joshua Ibrom <joshuaibrom@gmail.com>
2026-08-28 17:07:55 -0500

    fix(cli): use correct module
    This change was supposed to be bundled in with ab076d3.

--- crates/struktur-cli/src/main.rs
+++ crates/struktur-cli/src/main.rs
@@ -8,7 +8,8 @@ pub fn run(cli: Cli) {
     match &cli.command {
         Commands::Init => {
             // TODO: Handle error better
-            struktur_core::ensure_project_files().expect("project files should be creatable");
+            struktur_core::storage::ensure_project_files()
+                .expect("project files should be creatable");
             println!("Created project files."); // TODO: Better logging
         }
     }
